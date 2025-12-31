8a282e756070b228f7734372ec88e0de484c24df
fatihadanur1967-cyber <fatihadanur1967@gmail.com>
2025-12-31 16:47:49 +0300

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,39 @@
+// script.js dosyasının içi:
+
+const yaziAlani = document.getElementById('degisen-yazi');
+const kelimeler = ["C Programcısıyım", "Futbol Meraklısıyım", "Bilgisayar Mühendisiyim", "Web Geliştiriciyim"];
+
+let kelimeSirasi = 0;
+let harfSirasi = 0;
+let siliniyor = false;
+
+function yazveSil() {
+    const suankiKelime = kelimeler[kelimeSirasi];
+
+    if (siliniyor) {
+        yaziAlani.textContent = suankiKelime.substring(0, harfSirasi - 1);
+        harfSirasi--;
+    } else {
+        yaziAlani.textContent = suankiKelime.substring(0, harfSirasi + 1);
+        harfSirasi++;
+    }
+
+    let hiz = 100; 
+
+    if (siliniyor) hiz /= 2; 
+
+    if (!siliniyor && harfSirasi === suankiKelime.length) {
+      
+        hiz = 2000; 
+        siliniyor = true;
+    } else if (siliniyor && harfSirasi === 0) {
+       
+        siliniyor = false;
+        kelimeSirasi++;
+        if (kelimeSirasi === kelimeler.length) kelimeSirasi = 0;
+    }
+
+    setTimeout(yazveSil, hiz);
+}
+
+document.addEventListener('DOMContentLoaded', yazveSil);
